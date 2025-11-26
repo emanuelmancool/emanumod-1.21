a79bb2e4954aa9c958081c9be40f86b1fe87930c
Emanuel Cibaric <emanuelcibaric1@gmail.com>
2025-11-26 16:35:30 +0100

    REAL initial commit

---
+++ src/main/java/net/emanuel/emanumod/Emanumod.java
@@ -0,0 +1,17 @@
+package net.emanuel.emanumod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Emanumod implements ModInitializer {
+	public static final String MOD_ID = "emanumod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/emanuel/emanumod/EmanumodClient.java
@@ -0,0 +1,10 @@
+package net.emanuel.emanumod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmanumodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
