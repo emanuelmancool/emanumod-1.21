2ee9488221aa889380fdf59186573875e37ae7d0
Emanuel Cibaric <emanuelcibaric1@gmail.com>
2025-11-26 16:43:24 +0100

    added comment to test commit

--- src/main/java/net/emanuel/emanumod/Emanumod.java
+++ src/main/java/net/emanuel/emanumod/Emanumod.java
@@ -7,7 +7,7 @@ import org.slf4j.LoggerFactory;
 
 public class Emanumod implements ModInitializer {
 	public static final String MOD_ID = "emanumod";
-
+// not important comment
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
