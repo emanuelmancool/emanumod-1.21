2255fdbc2306dbc6d4848b3398e4da2a466a7a9d
Emanuel Cibaric <emanuelcibaric1@gmail.com>
2025-11-30 11:00:02 +0100

    added cardboard items

--- src/main/java/net/emanuel/emanumod/Emanumod.java
+++ src/main/java/net/emanuel/emanumod/Emanumod.java
@@ -1,5 +1,6 @@
 package net.emanuel.emanumod;
 
+import net.emanuel.emanumod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
@@ -12,6 +13,6 @@ public class Emanumod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-
+        ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/emanuel/emanumod/item/ModItems.java
@@ -0,0 +1,27 @@
+package net.emanuel.emanumod.item;
+
+import net.emanuel.emanumod.Emanumod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+    public static final Item CARDBOARD = registerItem("cardboard", new Item(new Item.Settings()));
+    public static final Item CARDBOARD_ROLL = registerItem("cardboard_roll", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(Emanumod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        Emanumod.LOGGER.info("Registering items for " + Emanumod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(fabricItemGroupEntries -> {
+            fabricItemGroupEntries.add(CARDBOARD);
+            fabricItemGroupEntries.add(CARDBOARD_ROLL);
+        });
+    }
+}
