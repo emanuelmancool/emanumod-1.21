038e22cbd564efc5a45fb097d01bf2d800fca596
Emanuel Cibaric <emanuelcibaric1@gmail.com>
2026-01-13 22:42:27 +0100

    made sandwich better

--- src/main/java/net/emanuel/emanumod/item/ModFoodComponents.java
+++ src/main/java/net/emanuel/emanumod/item/ModFoodComponents.java
@@ -5,7 +5,7 @@ import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 
 public class ModFoodComponents {
-    public static final FoodComponent GREEN_SANDWICH = new FoodComponent.Builder().nutrition(8).saturationModifier(0.5f)
+    public static final FoodComponent GREEN_SANDWICH = new FoodComponent.Builder().nutrition(12).saturationModifier(0.8f)
             .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 200), 0.25f).build();
 
 }
